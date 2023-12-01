c49958fa264ada7f8e2cde75005e9f8cb8509311
x93125tp <thomas.prior@student.manchester.ac.uk>
2023-12-01 10:05:21 +0000

    Stashing some changes before checking out a past tag

---
+++ src/Ex3/asm/finished_programs/light_seq_v1_2.s
@@ -0,0 +1,740 @@
+
+
+	ORG  0
+
+
+flash_130
+ADD max
+STA &0FFF
+LDA m_pattern_a
+STA &FFA
+LDA zero
+
+
+
+LDA delay_max
+loop_delay_130_0_0
+sub one
+JNE loop_delay_130_0_0
+
+LDA delay_max
+loop_delay_130_0_1
+sub one
+JNE loop_delay_130_0_1
+
+LDA delay_max
+loop_delay_130_0_2
+sub one
+JNE loop_delay_130_0_2
+
+LDA delay_max
+loop_delay_130_0_3
+sub one
+JNE loop_delay_130_0_3
+
+LDA delay_max
+loop_delay_130_0_4
+sub one
+JNE loop_delay_130_0_4
+
+LDA delay_max
+loop_delay_130_0_5
+sub one
+JNE loop_delay_130_0_5
+
+LDA delay_max
+loop_delay_130_0_6
+sub one
+JNE loop_delay_130_0_6
+
+LDA delay_max
+loop_delay_130_0_7
+sub one
+JNE loop_delay_130_0_7
+
+LDA delay_max
+loop_delay_130_0_8
+sub one
+JNE loop_delay_130_0_8
+
+LDA delay_max
+loop_delay_130_0_9
+sub one
+JNE loop_delay_130_0_9
+
+LDA delay_max
+loop_delay_130_0_10
+sub one
+JNE loop_delay_130_0_10
+
+LDA delay_max
+loop_delay_130_0_11
+sub one
+JNE loop_delay_130_0_11
+
+LDA delay_max
+loop_delay_130_0_12
+sub one
+JNE loop_delay_130_0_12
+
+LDA delay_max
+loop_delay_130_0_13
+sub one
+JNE loop_delay_130_0_13
+
+LDA delay_max
+loop_delay_130_0_14
+sub one
+JNE loop_delay_130_0_14
+
+LDA delay_max
+loop_delay_130_0_15
+sub one
+JNE loop_delay_130_0_15
+
+
+
+STA &0FFF
+
+LDA delay_max
+loop_delay_130_1_0
+sub one
+JNE loop_delay_130_1_0
+
+LDA delay_max
+loop_delay_130_1_1
+sub one
+JNE loop_delay_130_1_1
+
+LDA delay_max
+loop_delay_130_1_2
+sub one
+JNE loop_delay_130_1_2
+
+LDA delay_max
+loop_delay_130_1_3
+sub one
+JNE loop_delay_130_1_3
+
+LDA delay_max
+loop_delay_130_1_4
+sub one
+JNE loop_delay_130_1_4
+
+LDA delay_max
+loop_delay_130_1_5
+sub one
+JNE loop_delay_130_1_5
+
+LDA delay_max
+loop_delay_130_1_6
+sub one
+JNE loop_delay_130_1_6
+
+LDA delay_max
+loop_delay_130_1_7
+sub one
+JNE loop_delay_130_1_7
+
+LDA delay_max
+loop_delay_130_1_8
+sub one
+JNE loop_delay_130_1_8
+
+LDA delay_max
+loop_delay_130_1_9
+sub one
+JNE loop_delay_130_1_9
+
+LDA delay_max
+loop_delay_130_1_10
+sub one
+JNE loop_delay_130_1_10
+
+LDA delay_max
+loop_delay_130_1_11
+sub one
+JNE loop_delay_130_1_11
+
+LDA delay_max
+loop_delay_130_1_12
+sub one
+JNE loop_delay_130_1_12
+
+LDA delay_max
+loop_delay_130_1_13
+sub one
+JNE loop_delay_130_1_13
+
+LDA delay_max
+loop_delay_130_1_14
+sub one
+JNE loop_delay_130_1_14
+
+LDA delay_max
+loop_delay_130_1_15
+sub one
+JNE loop_delay_130_1_15
+
+
+
+
+
+
+flash_129
+ADD max
+STA &0FFF
+LDA m_pattern_b
+STA &FF9
+LDA zero
+
+
+
+LDA delay_max
+loop_delay_129_0_0
+sub one
+JNE loop_delay_129_0_0
+
+LDA delay_max
+loop_delay_129_0_1
+sub one
+JNE loop_delay_129_0_1
+
+LDA delay_max
+loop_delay_129_0_2
+sub one
+JNE loop_delay_129_0_2
+
+LDA delay_max
+loop_delay_129_0_3
+sub one
+JNE loop_delay_129_0_3
+
+LDA delay_max
+loop_delay_129_0_4
+sub one
+JNE loop_delay_129_0_4
+
+LDA delay_max
+loop_delay_129_0_5
+sub one
+JNE loop_delay_129_0_5
+
+LDA delay_max
+loop_delay_129_0_6
+sub one
+JNE loop_delay_129_0_6
+
+LDA delay_max
+loop_delay_129_0_7
+sub one
+JNE loop_delay_129_0_7
+
+
+
+STA &0FFF
+
+LDA delay_max
+loop_delay_129_1_0
+sub one
+JNE loop_delay_129_1_0
+
+LDA delay_max
+loop_delay_129_1_1
+sub one
+JNE loop_delay_129_1_1
+
+LDA delay_max
+loop_delay_129_1_2
+sub one
+JNE loop_delay_129_1_2
+
+LDA delay_max
+loop_delay_129_1_3
+sub one
+JNE loop_delay_129_1_3
+
+LDA delay_max
+loop_delay_129_1_4
+sub one
+JNE loop_delay_129_1_4
+
+LDA delay_max
+loop_delay_129_1_5
+sub one
+JNE loop_delay_129_1_5
+
+LDA delay_max
+loop_delay_129_1_6
+sub one
+JNE loop_delay_129_1_6
+
+LDA delay_max
+loop_delay_129_1_7
+sub one
+JNE loop_delay_129_1_7
+
+
+
+
+
+
+flash_128
+ADD max
+STA &0FFF
+LDA m_pattern_a
+STA &FF8
+LDA zero
+
+
+
+LDA delay_max
+loop_delay_128_0_0
+sub one
+JNE loop_delay_128_0_0
+
+LDA delay_max
+loop_delay_128_0_1
+sub one
+JNE loop_delay_128_0_1
+
+LDA delay_max
+loop_delay_128_0_2
+sub one
+JNE loop_delay_128_0_2
+
+LDA delay_max
+loop_delay_128_0_3
+sub one
+JNE loop_delay_128_0_3
+
+
+
+STA &0FFF
+
+LDA delay_max
+loop_delay_128_1_0
+sub one
+JNE loop_delay_128_1_0
+
+LDA delay_max
+loop_delay_128_1_1
+sub one
+JNE loop_delay_128_1_1
+
+LDA delay_max
+loop_delay_128_1_2
+sub one
+JNE loop_delay_128_1_2
+
+LDA delay_max
+loop_delay_128_1_3
+sub one
+JNE loop_delay_128_1_3
+
+
+
+
+
+
+flash_127
+ADD max
+STA &0FFF
+LDA m_pattern_b
+STA &FF7
+LDA zero
+
+
+
+LDA delay_max
+loop_delay_127_0_0
+sub one
+JNE loop_delay_127_0_0
+
+LDA delay_max
+loop_delay_127_0_1
+sub one
+JNE loop_delay_127_0_1
+
+
+
+STA &0FFF
+
+LDA delay_max
+loop_delay_127_1_0
+sub one
+JNE loop_delay_127_1_0
+
+LDA delay_max
+loop_delay_127_1_1
+sub one
+JNE loop_delay_127_1_1
+
+
+
+
+
+
+flash_126
+ADD max
+STA &0FFF
+LDA m_pattern_a
+STA &FF6
+LDA zero
+
+
+
+LDA delay_max
+loop_delay_126_0_0
+sub one
+JNE loop_delay_126_0_0
+
+
+
+STA &0FFF
+
+LDA delay_max
+loop_delay_126_1_0
+sub one
+JNE loop_delay_126_1_0
+
+
+
+
+
+
+flash_125
+ADD max
+STA &0FFF
+LDA m_pattern_b
+STA &FF5
+LDA zero
+
+
+
+LDA delay_max
+loop_delay_125_0_0
+sub one
+JNE loop_delay_125_0_0
+
+
+
+STA &0FFF
+
+LDA delay_max
+loop_delay_125_1_0
+sub one
+JNE loop_delay_125_1_0
+
+
+
+
+
+
+flash_124
+ADD max
+STA &0FFF
+
+
+LDA delay_max
+loop_delay_124_0_0
+sub one
+JNE loop_delay_124_0_0
+
+
+
+STA &0FFF
+
+LDA delay_max
+loop_delay_124_1_0
+sub one
+JNE loop_delay_124_1_0
+
+
+
+STA 4090
+
+
+flash_123
+ADD max
+STA &0FFF
+
+
+LDA delay_max
+loop_delay_123_0_0
+sub one
+JNE loop_delay_123_0_0
+
+
+
+STA &0FFF
+
+LDA delay_max
+loop_delay_123_1_0
+sub one
+JNE loop_delay_123_1_0
+
+
+
+STA 4089
+
+
+flash_122
+ADD max
+STA &0FFF
+
+
+LDA delay_max
+loop_delay_122_0_0
+sub one
+JNE loop_delay_122_0_0
+
+
+
+STA &0FFF
+
+LDA delay_max
+loop_delay_122_1_0
+sub one
+JNE loop_delay_122_1_0
+
+
+
+STA 4088
+
+
+flash_121
+ADD max
+STA &0FFF
+
+
+LDA delay_max
+loop_delay_121_0_0
+sub one
+JNE loop_delay_121_0_0
+
+
+
+STA &0FFF
+
+LDA delay_max
+loop_delay_121_1_0
+sub one
+JNE loop_delay_121_1_0
+
+
+
+STA 4087
+
+
+flash_120
+ADD max
+STA &0FFF
+
+
+LDA delay_max
+loop_delay_120_0_0
+sub one
+JNE loop_delay_120_0_0
+
+
+
+STA &0FFF
+
+LDA delay_max
+loop_delay_120_1_0
+sub one
+JNE loop_delay_120_1_0
+
+
+
+STA 4086
+
+
+flash_119
+ADD max
+STA &0FFF
+
+
+LDA delay_max
+loop_delay_119_0_0
+sub one
+JNE loop_delay_119_0_0
+
+
+
+STA &0FFF
+
+LDA delay_max
+loop_delay_119_1_0
+sub one
+JNE loop_delay_119_1_0
+
+
+
+STA 4085
+
+
+flash_118
+ADD max
+STA &0FFF
+
+
+LDA delay_max
+loop_delay_118_0_0
+sub one
+JNE loop_delay_118_0_0
+
+
+
+STA &0FFF
+
+LDA delay_max
+loop_delay_118_1_0
+sub one
+JNE loop_delay_118_1_0
+
+
+
+STA 4090
+
+
+flash_117
+ADD max
+STA &0FFF
+
+
+LDA delay_max
+loop_delay_117_0_0
+sub one
+JNE loop_delay_117_0_0
+
+
+
+STA &0FFF
+
+LDA delay_max
+loop_delay_117_1_0
+sub one
+JNE loop_delay_117_1_0
+
+
+
+STA 4089
+
+
+flash_116
+ADD max
+STA &0FFF
+
+
+LDA delay_max
+loop_delay_116_0_0
+sub one
+JNE loop_delay_116_0_0
+
+
+
+STA &0FFF
+
+LDA delay_max
+loop_delay_116_1_0
+sub one
+JNE loop_delay_116_1_0
+
+
+
+STA 4088
+
+
+flash_115
+ADD max
+STA &0FFF
+
+
+LDA delay_max
+loop_delay_115_0_0
+sub one
+JNE loop_delay_115_0_0
+
+
+
+STA &0FFF
+
+LDA delay_max
+loop_delay_115_1_0
+sub one
+JNE loop_delay_115_1_0
+
+
+
+STA 4087
+
+
+flash_114
+ADD max
+STA &0FFF
+
+
+LDA delay_max
+loop_delay_114_0_0
+sub one
+JNE loop_delay_114_0_0
+
+
+
+STA &0FFF
+
+LDA delay_max
+loop_delay_114_1_0
+sub one
+JNE loop_delay_114_1_0
+
+
+
+STA 4086
+
+
+flash_113
+ADD max
+STA &0FFF
+
+
+LDA delay_max
+loop_delay_113_0_0
+sub one
+JNE loop_delay_113_0_0
+
+
+
+STA &0FFF
+
+LDA delay_max
+loop_delay_113_1_0
+sub one
+JNE loop_delay_113_1_0
+
+
+
+STA 4085
+
+
+	done    JMP flash_130         ; Just in case stop instr fails
+
+
+test	DEFW &FFFF
+	
+one     DEFW 1           ; one
+max	DEFW &7FFF	 ; max ?
+neg     DEFW &8000       ; -max
+zero    DEFW &0000       ; zero
+delay	DEFW &000F	 ; 16
+	
+delay_1 DEFW &2ABD
+delay_2 DEFW &38FF
+delay_max DEFW &7FFF
+
+m_pattern_a DEFW &00ED
+m_pattern_b DEFW &00DB
+
+traffic DEFW &0FFF
